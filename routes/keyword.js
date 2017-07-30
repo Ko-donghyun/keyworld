63ca750012c4d251752ed1deef1b2d63835617e1
kodonghyun <kodonghyun1209@gmail.com>
2017-07-30 10:56:07 +0900

    Add second search route
    Add second search route to find the Keyword when it has previous relations.

--- routes/keyword.js
+++ routes/keyword.js
@@ -19,4 +19,21 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/** 
+  * To find the Keyword when it has previous relations 
+  */ 
+router.get('/extension', function(req, res, next) {
+   const previousKeyword = req.query.previousKeyword; 
+  const keyword = req.query.keyword;
+
+  return keywordController.extensionSearch(keyword, previousKeyword).then((result) => {
+    return res.json({
+      success: 1,
+      result,
+    })
+  }).catch((err) => {
+    return next(err);
+  });
+ });
+
 module.exports = router;
