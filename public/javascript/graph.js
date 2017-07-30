1dcb71a5f9dabaf3822b9a938a356f286e3d48b1
jung-inhyuk <jihsss@naver.com>
2017-07-29 19:44:13 -0700

    이전 검색어 표시

--- public/javascript/graph.js
+++ public/javascript/graph.js
@@ -2,91 +2,103 @@
 var network;
 var nodes, edges;
 var serachKeywords = [];
+var preKeyword;
 
 //Global options
 var options = {
-    nodes: {
-        shape: 'ellipse',
-        scaling: {
-            min: 10,
-            max: 25,
-            label: { min: 14, max: 30, drawThreshold: 9, maxVisible: 20 }
-        },
-        font: { size: 17, face: 'Helvetica Neue, Helvetica, Arial' }
+  nodes: {
+    shape: 'ellipse',
+    scaling: {
+      min: 10,
+      max: 25,
+      label: {min: 14, max: 30, drawThreshold: 9, maxVisible: 20}
     },
-    interaction: {
-        hover: true,
-        hoverConnectedEdges: false,
-        selectConnectedEdges: true
-    },
+    font: {size: 17, face: 'Helvetica Neue, Helvetica, Arial'}
+  },
+  interaction: {
+    hover: true,
+    hoverConnectedEdges: false,
+    selectConnectedEdges: true
+  },
 };
 makeNetwork();
 
-$(document).ready(function() {
-    $("#searchBtn").click(function() {
-        console.log("search button cliked");
-        var searchingWord = $("#searchInput").val();
-        search(searchingWord);
-    });
+$(document).ready(function () {
+  $("#searchBtn").click(function () {
+    console.log("search button cliked");
+    var searchingWord = $("#searchInput").val();
+    search(searchingWord);
+  });
 })
 
 function search(keyword) {
 
-    var jsonData;
-    var url = "http://localhost:3000/keyword?keyword=" + keyword;
+  var jsonData;
+  var url = "http://localhost:3000/keyword?keyword=" + keyword;
 
-    $.get(url, function(jqXHR) {}, 'json' /* xml, text, script, html */ )
-        .done(function(jqXHR) {})
-        .fail(function(jqXHR) {
-            alert("error");
-        })
-        .always(function(jqXHR) {
-            //alert("finished");
-            console.log(jqXHR);
-            jsonData = jqXHR;
-            setData(keyword, jsonData);
-            makeNetwork();
-        });
+  $.get(url, function (jqXHR) {
+  }, 'json' /* xml, text, script, html */)
+    .done(function (jqXHR) {
+    })
+    .fail(function (jqXHR) {
+      alert("error");
+    })
+    .always(function (jqXHR) {
+      //alert("finished");
+      console.log(jqXHR);
+      jsonData = jqXHR;
+      setData(keyword, jsonData);
+      makeNetwork();
+    });
 
 
 }
 
 function setData(upKeyword, jsonData) {
-    console.log(jsonData);
 
-    var nodeData = [];
-    var edgeData = [];
+  var nodeData = [];
+  var edgeData = [];
 
-    nodeData.push({ id: 0, label: upKeyword }, );
-    for (var i = 0; i < jsonData.result.length; i++) {
-        nodeData.push({ id: (i + 1), label: jsonData.result[i].label });
-        edgeData.push({ from: 0, to: (i + 1) });
+  nodeData.push({id: 0, label: upKeyword});
+  for (var i = 0; i < jsonData.result.length; i++) {
+    if (preKeyword !== jsonData.result[i].label) {
+      nodeData.push({id: (i + 1), label: jsonData.result[i].label});
+      edgeData.push({from: 0, to: (i + 1)});
     }
-    // create nodes
-    nodes = new vis.DataSet(nodeData);
-    // create an array with edges
-    edges = new vis.DataSet(edgeData);
+
+  }
+  if (preKeyword !== upKeyword && preKeyword !== undefined) {
+    nodeData.push({id: jsonData.result.length + 1, label: preKeyword, color: '#9f9faa'});
+    edgeData.push({from: 0, to: jsonData.result.length + 1, arrows: 'from', color: '#9f9faa'});
+  }
+  // create nodes
+  nodes = new vis.DataSet(nodeData);
+  // create an array with edges
+  edges = new vis.DataSet(edgeData);
+  preKeyword = upKeyword;
 }
 
 function makeNetwork() {
-    // create a network
-    var container = document.getElementById('mynetwork');
-    var data = {
-        nodes: nodes,
-        edges: edges
-    };
-    network = new vis.Network(container, data, options);
-    bindNetwork();
+  // create a network
+  var container = document.getElementById('mynetwork');
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+  network = new vis.Network(container, data, options);
+  bindNetwork();
 }
 
 function bindNetwork() {
-    network.on("click", expandEvent);
+  network.on("click", expandEvent);
 }
 
 function expandEvent(params) { // Expand a node (with event handler)
-    if (params.nodes.length) { //Did the click occur on a node?
-        var selectedKeyword = params.nodes[0]; //The id of the node clicked
-        var label = nodes.get(selectedKeyword).label
-        search(label);
+  if (params.nodes.length) { //Did the click occur on a node?
+    var selectedKeyword = params.nodes[0]; //The id of the node clicked
+    var label = nodes.get(selectedKeyword).label;
+    if (preKeyword !== label) {
+      search(label);
     }
+  }
 }
