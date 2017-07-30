dcbde4ef748b9f0fe17d0431b9a5a899d44e221d
Ko-donghyun <Ko-donghyun@users.noreply.github.com>
2017-07-29 17:44:02 -0700

    Keyword rest api (#2)
    * Remove unnecessary route file
    
    Remove users.js and related code that generated by express-generator.
    
    * Add Search for single keywords Controller
    
    Search keyword by one word.
    
    * Install mysql2 package
    
    For using sequelize.
    
    * Edit Error Routing
    
    When predictableError Occurred then catch reasonable error handling.
    
    * Add single search route
    
    Add single search route to find the Keyword when it has any other previous relations.
    
    * Edit line model
    
    Bottom column don't need to be necessary.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+const keywords = require('./routes/keyword');
 
 // Load Database model
 require('./model/generator.js');
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/keyword', keywords);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,6 +41,13 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  if (err.statusCode === 200) {
+    return res.json({
+      success: 0,
+      customCode: err.customCode,
+      message: err.message,
+    })
+  }
   // render the error page
   res.status(err.status || 500);
   res.render('error');
