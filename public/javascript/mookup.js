7f9453c228f947741393c33962aaefffa22df283
Daniel,Kim <skaghzz@naver.com>
2017-07-30 08:39:46 -0700

    Network namho (#8)
    * Add basic UI
    
    * first commit
    
    * second commit
    
    * 1단 검색 가능
    
    * ㅇㅇㅇㅇㅇㅇ
    
    * 이전 검색어 표시
    
    * ㄴㄴㄴㄴㄴㄴ
    
    * resolve comflict
    
    * 2-level keyword search
    
    complete 2-level keyword searching
    
    * add ‘add keyword’ button.
    
    * 2-level keyword add.
    
    * add reset
    
    doing reset after clicking search button
    
    * add search func.
    
    * Add delete modal.
    
    * Add delete function.
    
    * Modal error handling
    
    * node coloring & 3rd searching node => search icon

---
+++ public/javascript/mookup.js
@@ -0,0 +1,36 @@
+$(function() {
+    $("#searchBtnDELETE").click(function() {
+        var val = $("#searchInput").val();
+        var url = "http://ec2-52-78-185-227.ap-northeast-2.compute.amazonaws.com/verse/bible?version=kor&bibleName=1pet&startChapter=5&endChapter=5&startVerse=3&endVerse=4";
+        // $.post( url , {
+        //   value: val,
+        // }, function(jqXHR) {
+        //   alert( "success" );
+        // }, 'json' /* xml, text, script, html */)
+        //   .done(function(jqXHR) {
+        //     alert( "second success" );
+        //   })
+        //   .fail(function(jqXHR) {
+        //     alert( "error" );
+        //   })
+        //   .always(function(jqXHR) {
+        //     alert( "finished" );
+        //   });
+
+
+        $.get(url, function(jqXHR) {
+                alert("success");
+            }, 'json' /* xml, text, script, html */ )
+            .done(function(jqXHR) {
+                alert("second success");
+            })
+            .fail(function(jqXHR) {
+                alert("error");
+            })
+            .always(function(jqXHR) {
+                alert("finished");
+            });
+
+
+    })
+});
