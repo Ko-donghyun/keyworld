0ea2b9ec00f0e062499e4f2d4a9e932c5fbdee64
kodonghyun <kodonghyun1209@gmail.com>
2017-07-30 09:26:46 +0900

    Edit Error Routing
    When predictableError Occurred then catch reasonable error handling.

--- app.js
+++ app.js
@@ -41,6 +41,13 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  if (err.statusCode === 200) {
+    return res.json({
+      success: 0,
+      customCode: err.customCode,
+      message: err.message,
+    })
+  }
   // render the error page
   res.status(err.status || 500);
   res.render('error');
