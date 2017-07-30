c27aa4815d1aa9cf47d92ea8e247b6b8564d258e
jihsss <jihsss@naver.com>
2017-07-30 09:48:57 -0700

    Edit/design (#10)
    * Add basic UI
    
    * first commit
    
    * second commit
    
    * 1단 검색 가능
    
    * ㅇㅇㅇㅇㅇㅇ
    
    * 이전 검색어 표시
    
    * ㄴㄴㄴㄴㄴㄴ
    
    * resolve comflict
    
    * 2-level keyword search
    
    complete 2-level keyword searching
    
    * add ‘add keyword’ button.
    
    * 2-level keyword add.
    
    * add reset
    
    doing reset after clicking search button
    
    * add search func.
    
    * Add delete modal.
    
    * Add delete function.
    
    * Modal error handling
    
    * Add Design
    
    * fix Enter button to search
    
    * Edit node design
    
    * complete design
    
    * Edit Design

--- public/javascript/graph.js
+++ public/javascript/graph.js
@@ -7,84 +7,81 @@ var currentParams = null;
 
 //Global options
 var options = {
-    nodes: {
-        shape: 'ellipse',
-        scaling: {
-            min: 10,
-            max: 25,
-            label: { min: 14, max: 20, drawThreshold: 9, maxVisible: 20 }
-        },
-        font: { size: 17, face: 'Helvetica Neue, Helvetica, Arial' }
+  nodes: {
+    shape: 'circle',
+    scaling: {
+      min: 10,
+      max: 25,
+      label: {min: 14, max: 30, drawThreshold: 9, maxVisible: 20}
     },
-    interaction: {
-        hover: true,
-        hoverConnectedEdges: false,
-        selectConnectedEdges: true
-    },
+    font: {size: 25, face: 'Helvetica Neue, Helvetica, Arial'}
+  },
+  interaction: {
+    hover: true,
+    hoverConnectedEdges: false,
+    selectConnectedEdges: true
+  },
 };
 makeNetwork();
 
 function reset() {
-    preKeyword = undefined;
-    keyword = undefined;
-    searchKeywords = [];
+  preKeyword = undefined;
+  keyword = undefined;
+  searchKeywords = [];
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('#searchInput').keypress(function(e) {
-        if (e.which == '13') {
-            e.preventDefault();
-            var searchingWord = $("#searchInput").val();
-            $("#searchInput").blur();
-            search(searchingWord);
-        }
-    });
-    $("#searchBtn").click(function() {
-        reset();
-        console.log("search button cliked");
-        var searchingWord = $("#searchInput").val();
-        search(searchingWord);
-    });
+  $('#searchInput').keypress(function(e) {
+    if (e.which == '13') {
+      e.preventDefault();
+      var searchingWord = $("#searchInput").val();
+      $("#searchInput").blur();
+      reset();
+      search(searchingWord);
+    }
+  });
+  $("#searchBtn").click(function () {
+    reset();
+    var searchingWord = $("#searchInput").val();
+    search(searchingWord);
+  });
 })
 
 function search(keyword) {
-    console.log(preKeyword);
-    console.log(keyword);
-    if (keyword === searchKeywords[0]) {
-        searchKeywords[0] = undefined;
-        searchKeywords[1] = keyword;
-        $("#searchInput").val(keyword);
-    } else if (preKeyword !== undefined) {
-        searchKeywords[0] = preKeyword;
-        searchKeywords[1] = keyword;
-        $("#searchInput").val(preKeyword + ", " + keyword);
-    } else {
-        searchKeywords[1] = keyword;
-        $("#searchInput").val(keyword);
-    }
-    console.log(searchKeywords);
-    var jsonData;
-    if (searchKeywords[0] != undefined) {
-        url = "http://localhost:3000/keyword/extension?keyword=" + searchKeywords[1] + "&previousKeyword=" + searchKeywords[0];
-    } else {
-        url = "http://localhost:3000/keyword?keyword=" + searchKeywords[1];
-    }
+  if (keyword === searchKeywords[0]) {
+    searchKeywords[0] = undefined;
+    searchKeywords[1] = keyword;
+    $("#searchInput").val(keyword);
+  } else if (preKeyword !== undefined) {
+    searchKeywords[0] = preKeyword;
+    searchKeywords[1] = keyword;
+    $("#searchInput").val(preKeyword + ", " + keyword);
+  } else {
+    searchKeywords[1] = keyword;
+    $("#searchInput").val(keyword);
+  }
+  var jsonData;
+  if (searchKeywords[0] != undefined) {
+    url = "/keyword/extension?keyword=" + searchKeywords[1] + "&previousKeyword=" + searchKeywords[0];
+  } else {
+    url = "/keyword?keyword=" + searchKeywords[1];
+  }
 
-    $.get(url, function(jqXHR) {}, 'json' /* xml, text, script, html */ )
-        .done(function(jqXHR) {})
-        .fail(function(jqXHR) {
-            alert("error");
-        })
-        .always(function(jqXHR) {
-            //alert("finished");
-            jsonData = jqXHR;
+  $.get(url, function (jqXHR) {
+  }, 'json' /* xml, text, script, html */)
+    .done(function (jqXHR) {
+    })
+    .fail(function (jqXHR) {
+      alert("error");
+    })
+    .always(function (jqXHR) {
+      //alert("finished");
+      jsonData = jqXHR;
 
-            setData(keyword, jsonData);
-            makeNetwork();
-        });
-
-
+      setData(keyword, jsonData);
+      makeNetwork();
+    });
 }
 
 function setData(upKeyword, jsonData) {
@@ -93,11 +90,9 @@ function setData(upKeyword, jsonData) {
     } else {
         addDataNode(upKeyword, jsonData);
     }
-
 }
 
 function addSearchNode(upKeyword, jsonData) {
-    console.log("aaa");
     var nodeData = [];
     var edgeData = [];
 
@@ -139,28 +134,37 @@ function addDataNode(upKeyword, jsonData) {
     var nodeData = [];
     var edgeData = [];
 
-    nodeData.push({ id: 0, label: upKeyword, color: '#D2691E' });
-    for (var i = 0; i < jsonData.result.length; i++) {
-        nodeData.push({
-            id: (i + 1),
-            label: jsonData.result[i].label
-        });
-        edgeData.push({
-            from: 0,
-            to: (i + 1),
-            size: 30,
-            scaling: { min: 5 },
-        });
+  var nodeSize = 0;
+  var longest = 0;
+  var needSpace = 0;
+  var labelTemp = '';
+  nodeData.push({id: 0, label: upKeyword});
+  for (var i = 0, ii = jsonData.result.length; i < ii; i++) {
+    longest = longest < jsonData.result[i].label.length ? jsonData.result[i].label.length : longest;
+  }
+  nodeSize = 25 - longest;
+  for (var i = 0, ii = jsonData.result.length; i < ii; i++) {
+    labelTemp = jsonData.result[i].label;
+    needSpace = Math.ceil((longest - labelTemp.length)/2);
+    for (var j = 0; j < needSpace; j++){
+      labelTemp = ' ' + labelTemp + ' ';
     }
-    if (searchKeywords[0] !== upKeyword && searchKeywords[0] !== undefined) {
-        nodeData.push({ id: jsonData.result.length + 1, label: preKeyword, color: '#9f9faa' });
-        edgeData.push({ from: 0, to: jsonData.result.length + 1, arrows: 'from', color: '#9f9faa' });
+    if (needSpace > 3) {
+      labelTemp = '  ' + labelTemp + ' ';
     }
-    // create nodes
-    nodes = new vis.DataSet(nodeData);
-    // create an array with edges
-    edges = new vis.DataSet(edgeData);
-    preKeyword = upKeyword;
+    nodeData.push({id: (i + 1), label: labelTemp, font:{size: nodeSize}});
+    edgeData.push({from: 0, to: (i + 1)});
+    needSpace = 0;
+  }
+  if (searchKeywords[0] !== upKeyword && searchKeywords[0] !== undefined) {
+    nodeData.push({id: jsonData.result.length + 1, label: preKeyword, color: '#9f9faa'});
+    edgeData.push({from: 0, to: jsonData.result.length + 1, arrows: 'from', color: '#9f9faa'});
+  }
+  // create nodes
+  nodes = new vis.DataSet(nodeData);
+  // create an array with edges
+  edges = new vis.DataSet(edgeData);
+  preKeyword = upKeyword;
 }
 
 function makeNetwork() {
@@ -182,100 +186,99 @@ $("#mynetwork").contextmenu(function(e) {
 
 
 function bindNetwork() {
-    network.on("click", expandEvent);
-    network.on("oncontext", rightMouseClickEvent);
-    network.on("hoverNode", function(params) {
-        currentParams = params;
-    });
-    network.on("blurNode", function(params) {
-        currentParams = null;
-    });
+  network.on("click", expandEvent);
+  network.on("oncontext", rightMouseClickEvent);
+  network.on("hoverNode", function (params) {
+    currentParams = params;
+  });
+  network.on("blurNode", function (params) {
+    currentParams = null;
+  });
 }
 
 function expandEvent(params) { // Expand a node (with event handler)
-    if (params.edges.length === 0) return;
-    var selectedKeyword = params.nodes[0]; //The id of the node clicked
-    var label = nodes.get(selectedKeyword).label;
-    console.log(params);
-    if (searchKeywords[0] !== undefined && searchKeywords[1] !== undefined && searchKeywords[0] !== label && searchKeywords[1] !== label) {
-        //구글로 이동
-        //https://www.google.com/search?q=apple+ipad+jobs&oq=apple+ipad+jobs
-        src = "https://www.google.com/search?q=" + searchKeywords[0] + "+" + searchKeywords[1] + "+" + label +
-            "&oq=" + searchKeywords[0] + "+" + searchKeywords[1] + "+" + label;
-        location.href = src;
-    } else if (params.nodes.length) { //Did the click occur on a node?
+  if (params.edges.length === 0) return;
 
-        if (preKeyword !== label) {
-            search(label);
-        }
+  var selectedKeyword = params.nodes[0]; //The id of the node clicked
+  if (nodes.get(selectedKeyword) === null) return;
+  var label = nodes.get(selectedKeyword).label;
+  label = label.trim();
+  if (searchKeywords[0] !== undefined && searchKeywords[1] !== undefined && searchKeywords[0] !== label && searchKeywords[1] !== label) {
+    //구글로 이동
+    //https://www.google.com/search?q=apple+ipad+jobs&oq=apple+ipad+jobs
+    src = "https://www.google.com/search?q=" + searchKeywords[0] + "+" + searchKeywords[1] + "+" + label +
+      "&oq=" + searchKeywords[0] + "+" + searchKeywords[1] + "+" + label;
+    location.href = src;
+  } else if (params.nodes.length) { //Did the click occur on a node?
+
+    if (preKeyword !== label) {
+      search(label);
     }
+  }
 }
 
 function rightMouseClickEvent(params) {
-    if (currentParams === null) return;
-    var tempNodes = nodes.get();
-    for (var i = 0, ii = nodes.length; i < ii; i++) {
-        if (currentParams.node == tempNodes[i].id) {
-            selectedKeyword = tempNodes[i].label;
-        }
+  if (currentParams === null) return;
+  var tempNodes = nodes.get();
+  for(var i = 0, ii = nodes.length; i < ii; i ++) {
+    if (currentParams.node == tempNodes[i].id) {
+      selectedKeyword = tempNodes[i].label;
     }
-    if (selectedKeyword === searchKeywords[1]) {
-        return;
-    }
-    $("#askWord").html(selectedKeyword);
-    $('#delModal').modal('show');
+  }
+  if (selectedKeyword === searchKeywords[1]) {
+    return;
+  }
+  $("#askWord").html(selectedKeyword);
+  $('#delModal').modal('show');
 }
 
-$("#addKeywordBtn").click(function() {
-    var url = "http://localhost:3000/keyword";
-    var val = $("#addKeywordVal").val();
-    var obj = { keyword: preKeyword, newKeyword: val };
+$("#addKeywordBtn").click(function () {
+  var url = "/keyword";
+  var val = $("#addKeywordVal").val();
+  var obj = {keyword: preKeyword, newKeyword: val};
 
-    console.log(searchKeywords[0]);
 
-    if (searchKeywords[0] !== undefined) {
-        url += '/extension';
-        obj = { previousKeyword: searchKeywords[0], keyword: preKeyword, newKeyword: val };
-    }
-    $.post(url, obj, function(jqXHR) {
-            //success
-        }, 'json' /* xml, text, script, html */ )
-        .done(function(jqXHR) {
-            var node = { id: nodes.length + 1, label: val };
-            var edge = { from: 0, to: nodes.length + 1 };
-            nodes.add(node);
-            edges.add(edge);
-        })
-        .fail(function(jqXHR) {
-            alert("error");
-        })
-        .always(function(jqXHR) {
-            $('#addModal').modal('toggle');
-        });
+  if (searchKeywords[0] !== undefined) {
+    url += '/extension';
+    obj = {previousKeyword: searchKeywords[0], keyword: preKeyword, newKeyword: val};
+  }
+  $.post(url, obj, function (jqXHR) {
+    //success
+  }, 'json' /* xml, text, script, html */)
+    .done(function (jqXHR) {
+      var node = {id: nodes.length + 1, label: val};
+      var edge = {from: 0, to: nodes.length + 1};
+      nodes.add(node);
+      edges.add(edge);
+    })
+    .fail(function (jqXHR) {
+      alert("error");
+    })
+    .always(function (jqXHR) {
+      $('#addModal').modal('toggle');
+    });
 });
 
 $("#delKeywordBtn").click(function() {
-    var url = "http://localhost:3000/keyword/report";
-    var val = $("#askWord").text();
-    var email = $("#forDelEmail").val();
-    var obj = { ancestorKeyword: searchKeywords[0], parentKeyword: preKeyword, keyword: val, email: email };
+  var url = "/keyword/report";
+  var val = $("#askWord").text();
+  var email = $("#forDelEmail").val();
+  var obj = {ancestorKeyword: searchKeywords[0], parentKeyword: preKeyword, keyword: val, email: email};
 
-    $.post(url, obj, function(jqXHR) {
-            //success
-        }, 'json' /* xml, text, script, html */ )
-        .done(function(jqXHR) {
-            console.log("delete?");
-        })
-        .fail(function(jqXHR) {
-            alert("error");
-        })
-        .always(function(jqXHR) {
-            $('#addModal').modal('toggle');
-        });
+  $.post(url, obj, function (jqXHR) {
+    //success
+  }, 'json' /* xml, text, script, html */)
+    .done(function (jqXHR) {
+    })
+    .fail(function (jqXHR) {
+      alert("error");
+    })
+    .always(function (jqXHR) {
+      $('#delModal').modal('toggle');
+    });
 });
 
-$('.modal').on('hidden.bs.modal', function(e) {
-    console.log('modal close');
-    $("#forDelEmail").val("");
-    $("#addKeywordVal").val("");
+$('.modal').on('hidden.bs.modal', function (e) {
+  $("#forDelEmail").val("");
+  $("#addKeywordVal").val("");
 });
