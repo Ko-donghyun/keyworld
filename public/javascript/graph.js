e884490bc250bd2ec4a667f933b24772c64d7b55
Daniel Kim <skaghzz@naver.com>
2017-07-30 11:49:57 -0700

    edit adding keyword event error

--- public/javascript/graph.js
+++ public/javascript/graph.js
@@ -8,82 +8,80 @@ var nodeSize = 0;
 
 //Global options
 var options = {
-  nodes: {
-    shape: 'circle',
-    scaling: {
-      min: 10,
-      max: 25,
-      label: {min: 14, max: 30, drawThreshold: 9, maxVisible: 20}
+    nodes: {
+        shape: 'circle',
+        scaling: {
+            min: 10,
+            max: 25,
+            label: { min: 14, max: 30, drawThreshold: 9, maxVisible: 20 }
+        },
+        font: { size: 25, face: 'Helvetica Neue, Helvetica, Arial' }
     },
-    font: {size: 25, face: 'Helvetica Neue, Helvetica, Arial'}
-  },
-  interaction: {
-    hover: true,
-    hoverConnectedEdges: false,
-    selectConnectedEdges: true
-  },
+    interaction: {
+        hover: true,
+        hoverConnectedEdges: false,
+        selectConnectedEdges: true
+    },
 };
 makeNetwork();
 
 function reset() {
-  preKeyword = undefined;
-  keyword = undefined;
-  searchKeywords = [];
+    preKeyword = undefined;
+    keyword = undefined;
+    searchKeywords = [];
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-  $('#searchInput').keypress(function(e) {
-    if (e.which == '13') {
-      e.preventDefault();
-      reset();
+    $('#searchInput').keypress(function(e) {
+        if (e.which == '13') {
+            e.preventDefault();
+            reset();
 
-      var searchingWord = $("#searchInput").val();
-      $("#searchInput").blur();
-      search(searchingWord);
-    }
-  });
-  $("#searchBtn").click(function () {
-    reset();
-    var searchingWord = $("#searchInput").val();
-    search(searchingWord);
-  });
+            var searchingWord = $("#searchInput").val();
+            $("#searchInput").blur();
+            search(searchingWord);
+        }
+    });
+    $("#searchBtn").click(function() {
+        reset();
+        var searchingWord = $("#searchInput").val();
+        search(searchingWord);
+    });
 })
 
 function search(keyword) {
-  if (keyword === searchKeywords[0]) {
-    searchKeywords[0] = undefined;
-    searchKeywords[1] = keyword;
-    $("#searchInput").val(keyword);
-  } else if (preKeyword !== undefined) {
-    searchKeywords[0] = preKeyword;
-    searchKeywords[1] = keyword;
-    $("#searchInput").val(preKeyword + ", " + keyword);
-  } else {
-    searchKeywords[1] = keyword;
-    $("#searchInput").val(keyword);
-  }
-  var jsonData;
-  if (searchKeywords[0] != undefined) {
-    url = "/keyword/extension?keyword=" + searchKeywords[1] + "&previousKeyword=" + searchKeywords[0];
-  } else {
-    url = "/keyword?keyword=" + searchKeywords[1];
-  }
+    if (keyword === searchKeywords[0]) {
+        searchKeywords[0] = undefined;
+        searchKeywords[1] = keyword;
+        $("#searchInput").val(keyword);
+    } else if (preKeyword !== undefined) {
+        searchKeywords[0] = preKeyword;
+        searchKeywords[1] = keyword;
+        $("#searchInput").val(preKeyword + ", " + keyword);
+    } else {
+        searchKeywords[1] = keyword;
+        $("#searchInput").val(keyword);
+    }
+    var jsonData;
+    if (searchKeywords[0] != undefined) {
+        url = "/keyword/extension?keyword=" + searchKeywords[1] + "&previousKeyword=" + searchKeywords[0];
+    } else {
+        url = "/keyword?keyword=" + searchKeywords[1];
+    }
 
-  $.get(url, function (jqXHR) {
-  }, 'json' /* xml, text, script, html */)
-    .done(function (jqXHR) {
-    })
-    .fail(function (jqXHR) {
-      alert("error");
-    })
-    .always(function (jqXHR) {
-      //alert("finished");
-      jsonData = jqXHR;
+    $.get(url, function(jqXHR) {}, 'json' /* xml, text, script, html */ )
+        .done(function(jqXHR) {})
+        .fail(function(jqXHR) {
+            alert("error");
+        })
+        .always(function(jqXHR) {
+            //alert("finished");
+            jsonData = jqXHR;
 
-      setData(keyword, jsonData);
-      makeNetwork();
-    });
+            setData(keyword, jsonData);
+            makeNetwork();
+        });
 }
 
 function setData(upKeyword, jsonData) {
@@ -98,9 +96,9 @@ function addSearchNode(upKeyword, jsonData) {
     var nodeData = [];
     var edgeData = [];
 
-  nodeData.push({id: 0, label: upKeyword, color: {background:'#ffffff', border:'#e68307', hover:{background:'#F59304',border:'#e68307'}, highlight:{background:'#F59304',border:'#e68307'}}});
+    nodeData.push({ id: 0, label: upKeyword, color: { background: '#ffffff', border: '#e68307', hover: { background: '#F59304', border: '#e68307' }, highlight: { background: '#F59304', border: '#e68307' } } });
 
-  for (var i = 0; i < jsonData.result.length; i++) {
+    for (var i = 0; i < jsonData.result.length; i++) {
         nodeData.push({
             id: (i + 1),
             shape: 'icon',
@@ -137,20 +135,20 @@ function addDataNode(upKeyword, jsonData) {
     var nodeData = [];
     var edgeData = [];
 
-  nodeData.push({id: 0, label: upKeyword, color: {background:'#ffffff', border:'#e68307', hover:{background:'#F59304',border:'#e68307'}, highlight:{background:'#F59304',border:'#e68307'}}});
-  for (var i = 0, ii = jsonData.result.length; i < ii; i++) {
-    nodeData.push({id: (i + 1), label: jsonData.result[i].label, font:{size: 15}, color: {background:'#ffffff', border:'#6d9b77', hover:{background:'#85BD91',border:'#6d9b77'}, highlight:{background:'#85BD91',border:'#6d9b77'}}});
-    edgeData.push({from: 0, to: (i + 1), color: '#6d9b77' });
-  }
-  if (searchKeywords[0] !== upKeyword && searchKeywords[0] !== undefined) {
-    nodeData.push({id: jsonData.result.length + 1, label: preKeyword, color: '#9f9faa'});
-    edgeData.push({from: 0, to: jsonData.result.length + 1, arrows: 'from', color: '#9f9faa'});
-  }
-  // create nodes
-  nodes = new vis.DataSet(nodeData);
-  // create an array with edges
-  edges = new vis.DataSet(edgeData);
-  preKeyword = upKeyword;
+    nodeData.push({ id: 0, label: upKeyword, color: { background: '#ffffff', border: '#e68307', hover: { background: '#F59304', border: '#e68307' }, highlight: { background: '#F59304', border: '#e68307' } } });
+    for (var i = 0, ii = jsonData.result.length; i < ii; i++) {
+        nodeData.push({ id: (i + 1), label: jsonData.result[i].label, font: { size: 15 }, color: { background: '#ffffff', border: '#6d9b77', hover: { background: '#85BD91', border: '#6d9b77' }, highlight: { background: '#85BD91', border: '#6d9b77' } } });
+        edgeData.push({ from: 0, to: (i + 1), color: '#6d9b77' });
+    }
+    if (searchKeywords[0] !== upKeyword && searchKeywords[0] !== undefined) {
+        nodeData.push({ id: jsonData.result.length + 1, label: preKeyword, color: '#9f9faa' });
+        edgeData.push({ from: 0, to: jsonData.result.length + 1, arrows: 'from', color: '#9f9faa' });
+    }
+    // create nodes
+    nodes = new vis.DataSet(nodeData);
+    // create an array with edges
+    edges = new vis.DataSet(edgeData);
+    preKeyword = upKeyword;
 }
 
 function makeNetwork() {
@@ -172,99 +170,110 @@ $("#mynetwork").contextmenu(function(e) {
 
 
 function bindNetwork() {
-  network.on("click", expandEvent);
-  network.on("oncontext", rightMouseClickEvent);
-  network.on("hoverNode", function (params) {
-    currentParams = params;
-  });
-  network.on("blurNode", function (params) {
-    currentParams = null;
-  });
+    network.on("click", expandEvent);
+    network.on("oncontext", rightMouseClickEvent);
+    network.on("hoverNode", function(params) {
+        currentParams = params;
+    });
+    network.on("blurNode", function(params) {
+        currentParams = null;
+    });
 }
 
 function expandEvent(params) { // Expand a node (with event handler)
-  if (params.edges.length === 0) return;
+    if (params.edges.length === 0) return;
 
-  var selectedKeyword = params.nodes[0]; //The id of the node clicked
-  if (nodes.get(selectedKeyword) === null) return;
-  var label = nodes.get(selectedKeyword).label;
-  label = label.trim();
-  if (searchKeywords[0] !== undefined && searchKeywords[1] !== undefined && searchKeywords[0] !== label && searchKeywords[1] !== label) {
-    //구글로 이동
-    //https://www.google.com/search?q=apple+ipad+jobs&oq=apple+ipad+jobs
-    src = "https://www.google.com/search?q=" + searchKeywords[0] + "+" + searchKeywords[1] + "+" + label +
-      "&oq=" + searchKeywords[0] + "+" + searchKeywords[1] + "+" + label;
-    location.href = src;
-  } else if (params.nodes.length) { //Did the click occur on a node?
+    var selectedKeyword = params.nodes[0]; //The id of the node clicked
+    if (nodes.get(selectedKeyword) === null) return;
+    var label = nodes.get(selectedKeyword).label;
+    label = label.trim();
+    if (searchKeywords[0] !== undefined && searchKeywords[1] !== undefined && searchKeywords[0] !== label && searchKeywords[1] !== label) {
+        //구글로 이동
+        //https://www.google.com/search?q=apple+ipad+jobs&oq=apple+ipad+jobs
+        src = "https://www.google.com/search?q=" + searchKeywords[0] + "+" + searchKeywords[1] + "+" + label +
+            "&oq=" + searchKeywords[0] + "+" + searchKeywords[1] + "+" + label;
+        var newWinodow = window.open("about:black");
+        newWinodow.location.href = src
+    } else if (params.nodes.length) { //Did the click occur on a node?
 
-    if (preKeyword !== label) {
-      search(label);
+        if (preKeyword !== label) {
+            search(label);
+        }
     }
-  }
 }
 
 function rightMouseClickEvent(params) {
-  if (currentParams === null) return;
-  var tempNodes = nodes.get();
-  for(var i = 0, ii = nodes.length; i < ii; i ++) {
-    if (currentParams.node == tempNodes[i].id) {
-      selectedKeyword = tempNodes[i].label;
+    if (currentParams === null) return;
+    var tempNodes = nodes.get();
+    for (var i = 0, ii = nodes.length; i < ii; i++) {
+        if (currentParams.node == tempNodes[i].id) {
+            selectedKeyword = tempNodes[i].label;
+        }
     }
-  }
-  if (selectedKeyword === searchKeywords[1]) {
-    return;
-  }
-  $("#askWord").html(selectedKeyword);
-  $('#delModal').modal('show');
+    if (selectedKeyword === searchKeywords[1]) {
+        return;
+    }
+    $("#askWord").html(selectedKeyword);
+    $('#delModal').modal('show');
 }
 
-$("#addKeywordBtn").click(function () {
-  var url = "/keyword";
-  var val = $("#addKeywordVal").val();
-  var obj = {keyword: preKeyword, newKeyword: val};
+$("#addKeywordBtn").click(function() {
+    var url = "/keyword";
+    var val = $("#addKeywordVal").val();
+    var obj = { keyword: preKeyword, newKeyword: val };
 
 
-  if (searchKeywords[0] !== undefined) {
-    url += '/extension';
-    obj = {previousKeyword: searchKeywords[0], keyword: preKeyword, newKeyword: val};
-  }
-  $.post(url, obj, function (jqXHR) {
-    //success
-  }, 'json' /* xml, text, script, html */)
-    .done(function (jqXHR) {
-      var node = {id: nodes.length + 1, label: val, font:{size: nodeSize}, color: {background:'#ffffff', border:'#6d9b77', hover:{background:'#85BD91',border:'#6d9b77'}, highlight:{background:'#85BD91',border:'#6d9b77'}}};
-      var edge = {from: 0, to: nodes.length + 1, color: '#6d9b77' };
-      nodes.add(node);
-      edges.add(edge);
-    })
-    .fail(function (jqXHR) {
-      alert("error");
-    })
-    .always(function (jqXHR) {
-      $('#addModal').modal('toggle');
-    });
+    if (searchKeywords[0] !== undefined) {
+        url += '/extension';
+        obj = { previousKeyword: searchKeywords[0], keyword: preKeyword, newKeyword: val };
+    }
+    $.post(url, obj, function(jqXHR) {
+            //success
+        }, 'json' /* xml, text, script, html */ )
+        .done(function(jqXHR) {
+            console.log(val);
+            var node = {
+                id: nodes.length + 1,
+                label: val,
+                font: { size: 15 },
+                color: {
+                    background: '#ffffff',
+                    border: '#6d9b77',
+                    hover: { background: '#85BD91', border: '#6d9b77' },
+                    highlight: { background: '#85BD91', border: '#6d9b77' }
+                }
+            };
+            var edge = { from: 0, to: nodes.length + 1, color: '#6d9b77' };
+            nodes.add(node);
+            edges.add(edge);
+        })
+        .fail(function(jqXHR) {
+            alert("error");
+        })
+        .always(function(jqXHR) {
+            $('#addModal').modal('toggle');
+        });
 });
 
 $("#delKeywordBtn").click(function() {
-  var url = "/keyword/report";
-  var val = $("#askWord").text();
-  var email = $("#forDelEmail").val();
-  var obj = {ancestorKeyword: searchKeywords[0], parentKeyword: preKeyword, keyword: val, email: email};
+    var url = "/keyword/report";
+    var val = $("#askWord").text();
+    var email = $("#forDelEmail").val();
+    var obj = { ancestorKeyword: searchKeywords[0], parentKeyword: preKeyword, keyword: val, email: email };
 
-  $.post(url, obj, function (jqXHR) {
-    //success
-  }, 'json' /* xml, text, script, html */)
-    .done(function (jqXHR) {
-    })
-    .fail(function (jqXHR) {
-      alert("error");
-    })
-    .always(function (jqXHR) {
-      $('#delModal').modal('toggle');
-    });
+    $.post(url, obj, function(jqXHR) {
+            //success
+        }, 'json' /* xml, text, script, html */ )
+        .done(function(jqXHR) {})
+        .fail(function(jqXHR) {
+            alert("error");
+        })
+        .always(function(jqXHR) {
+            $('#delModal').modal('toggle');
+        });
 });
 
-$('.modal').on('hidden.bs.modal', function (e) {
-  $("#forDelEmail").val("");
-  $("#addKeywordVal").val("");
+$('.modal').on('hidden.bs.modal', function(e) {
+    $("#forDelEmail").val("");
+    $("#addKeywordVal").val("");
 });
